353b5b4fcf88aaefe933f1ad5463827d53dcfcf2
Jay OConnor <jay@bellycard.com>
2014-07-22 08:45:30 -0500

    active support concern when including in rails.

--- lib/napa_pagination/grape_helpers.rb
+++ lib/napa_pagination/grape_helpers.rb
@@ -21,7 +21,7 @@ module NapaPagination
       page      = params.try(:page) || 1
       per_page  = params.try(:per_page) || 25
 
-      order_by_params!(data) if data.is_a?(ActiveRecord::Relation) && data.size > 0 
+      order_by_params!(data) if data.is_a?(ActiveRecord::Relation) && data.size > 0
 
       if data.is_a?(Array)
         Kaminari.paginate_array(data).page(page).per(per_page)
@@ -31,14 +31,16 @@ module NapaPagination
     end
 
     def order_by_params!(data)
-      if data.column_names.map(&:to_sym).include?(params[:sort_by])
+      if params[:sort_by] && data.column_names.map(&:to_sym).include?(params[:sort_by].to_sym)
         sort_order = params.try(:sort_order) || :asc
-        data.order!(params[:sort_by] => sort_order)
+        data.order!(params[:sort_by] => sort_order.to_sym)
       end
       data
     end
 
     # extend all endpoints to include this
-    Grape::Endpoint.send :include, self
+    Grape::Endpoint.send :include, self if defined?(Grape)
+    # rails 4 controller concern
+    extend ActiveSupport::Concern if defined?(Rails)
   end
 end
